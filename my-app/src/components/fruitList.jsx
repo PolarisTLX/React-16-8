a4252be2b69071126aabea1c110d22f3a86f261e
Paul Rail <paul.a.rail@gmail.com>
2019-05-29 11:09:57 -0400

    converted FruitList from class to function style component

--- my-app/src/components/fruitList.jsx
+++ my-app/src/components/fruitList.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
-class FruitList extends Component {
-  // state = {}
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello From FruitList Component</h1>
-        <ol>
-          { this.props.fruits.map(element => <li>{element}</li>)}
-        </ol>
-      </div>
-    );
-  }
+// class FruitList extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello From FruitList Component</h1>
+//         <ol>
+//           { this.props.fruits.map(element => <li>{element}</li>)}
+//         </ol>
+//       </div>
+//     );
+//   }
+// }
+
+const FruitList = props => {
+
+  return (
+    <div className="App">
+      <h1>Hello From FruitList Component</h1>
+      <ol>
+        {props.fruits.map(element => <li>{element}</li>)}
+      </ol>
+    </div>
+  );
 }
 
 export default FruitList;
