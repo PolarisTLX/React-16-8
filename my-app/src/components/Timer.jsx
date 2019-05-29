1837a4ef40c54ff7a5453bd4c8bcb2518f621442
Paul Rail <paul.a.rail@gmail.com>
2019-05-29 11:39:16 -0400

    converted Timer component from class to functional component with Hooks and useState

--- my-app/src/components/Timer.jsx
+++ my-app/src/components/Timer.jsx
@@ -1,26 +1,49 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class Timer extends Component {
-  state = {
-    date: new Date()
-  };
+// class Timer extends Component {
+//   state = {
+//     date: new Date()
+//   };
 
-  callMe() {
-    setInterval(() => {      
-      // this.props.date = new Date();  // properties are immutable
-      this.setState({ date: new Date()});
+//   callMe() {
+//     setInterval(() => {      
+//       // this.props.date = new Date();  // properties are immutable
+//       this.setState({ date: new Date()});
+//     }, 1000);
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Current Time</h1>
+//         <h2>It is { this.state.date.toLocaleTimeString() }</h2>
+//         { this.callMe() }
+//       </div>
+//      );
+//   }
+// }
+
+// converted to functional component with useState
+const Timer = props => {
+
+  const [date, setDate] = useState([]);
+
+  const callMe = () => {
+    setInterval(() => {
+      let newDate = new Date().toLocaleTimeString();      
+      setDate(newDate);
     }, 1000);
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Current Time</h1>
-        <h2>It is { this.state.date.toLocaleTimeString() }</h2>
-        { this.callMe() }
-      </div>
-     );
-  }
+  return (
+    <div className="App">
+      <h1>Current Time</h1>
+      <h2>It is {date}</h2>
+      {callMe()}
+    </div>
+  )
 }
+
 
 export default Timer;
--- my-app/src/components/fruitList.jsx
+++ my-app/src/components/fruitList.jsx
@@ -13,13 +13,15 @@ import React, { Component } from 'react';
 //   }
 // }
 
+
+// converted to functional component:
 const FruitList = props => {
 
   return (
     <div className="App">
       <h1>Hello From FruitList Component</h1>
       <ol>
-        {props.fruits.map(element => <li>{element}</li>)}
+        {props.fruits.map((element, index) => <li key={index}>{element}</li>)}
       </ol>
     </div>
   );
